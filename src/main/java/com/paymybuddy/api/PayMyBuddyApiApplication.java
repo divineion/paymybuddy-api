838bcf705d92f23113ad5af1fa8b1b4b49337cd3
divineion <najima.guermoudi@gmail.com>
2025-04-25 22:38:43 +0200

    Initial commit
    Initialize Spring Boot project pmb-app (com.paymybuddy.api) with:
     - Spring Data JPA and H2 dependencies for the database management,
     - Spring Security and Thymeleaf for the authentication process and view
    rendering,
     - Spring Boot Web and log4j2 for the web layer and logging,
     - Spring Boot DevTools for hot reloading during development
    (hopefully),
     - Spring Boot Starter Test and Spring Security Test for
    unit/integration testing.

---
+++ src/main/java/com/paymybuddy/api/PayMyBuddyApiApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMyBuddyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMyBuddyApiApplication.class, args);
+	}
+
+}
