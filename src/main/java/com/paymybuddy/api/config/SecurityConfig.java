3a18527da203798a8e7c9cf5758967cedf86111d
divineion <najima.guermoudi@gmail.com>
2025-05-13 14:43:13 +0200

    WIP(security): disable CSRF and allow all requests for testing
    - temporarily allow all incoming requests to facilitate testing of the
    controller

---
+++ src/main/java/com/paymybuddy/api/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.paymybuddy.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration // indiquera à Spring qu'il y a des beans dans la classe
+@EnableWebSecurity //permet de configurer des éléments de sécurity
+public class SecurityConfig {
+	// permettre de modéliser une chaîne de filtres de sécurité
+	@Bean // enregistre la valeur de retour en tant que Bean 
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http
+	            .authorizeHttpRequests(auth -> auth
+	                    .anyRequest().permitAll()
+	                ).build();
+	}
+}
