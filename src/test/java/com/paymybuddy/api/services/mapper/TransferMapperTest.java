ec690d8c43392d9943c579b0c31f915dc6833b14
divineion <najima.guermoudi@gmail.com>
2025-05-14 16:54:42 +0200

    feat(test): add unit test for TransferMapper#fromTransferToTransferDto()
    Add tests to verify the returned TransferDto based on a Transfer entity:
     - when the current user is the sender (the amount should correspond to
    the Transfer's totalAmount),
     - when the current user is the receiver (the amount should correspond
    to the Transfer's amount).
    
    Mock User and Transfer object to simulate a transfer between users.

---
+++ src/test/java/com/paymybuddy/api/services/mapper/TransferMapperTest.java
@@ -0,0 +1,70 @@
+package com.paymybuddy.api.services.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.paymybuddy.api.model.Transfer;
+import com.paymybuddy.api.model.User;
+import com.paymybuddy.api.model.dto.TransferDto;
+
+@ExtendWith(MockitoExtension.class)
+public class TransferMapperTest {
+	private final TransferMapper mapper = new TransferMapper();
+	
+	@Test
+	public void testFromTransferToTransferDto_WhenUserIsSender_shouldReturnDtoWithTotalAmount() {
+		// GIVEN
+		User sender = mock(User.class);
+		when(sender.getId()).thenReturn(1);
+		when(sender.getUsername()).thenReturn("John");
+		
+		User receiver = mock(User.class);
+		when(receiver.getId()).thenReturn(2);
+		when(receiver.getUsername()).thenReturn("Jack");
+		
+		Transfer transfer = mock(Transfer.class);
+		when(transfer.getSender()).thenReturn(sender);
+		when(transfer.getReceiver()).thenReturn(receiver);
+		when(transfer.getTotalAmount()).thenReturn(new BigDecimal("100.5"));
+		
+		//WHEN
+		TransferDto dto = mapper.fromTransferToTransferDto(transfer, 1);
+		
+		BigDecimal expectedAmount = new BigDecimal("100.50");
+		
+		//THEN
+		assertEquals(expectedAmount, dto.amount());
+	}
+	
+	@Test
+	public void testFromTransferToTransferDto_WhenUserIsReceiver_shouldReturnDtoWithAmountExcludingFees() {
+		// GIVEN	
+		User sender = mock(User.class);
+		when(sender.getId()).thenReturn(1);
+		when(sender.getUsername()).thenReturn("John");
+		
+		User receiver = mock(User.class);
+		when(receiver.getId()).thenReturn(2);
+		when(receiver.getUsername()).thenReturn("Jack");
+		
+		Transfer transfer = mock(Transfer.class);
+		when(transfer.getSender()).thenReturn(sender);
+		when(transfer.getReceiver()).thenReturn(receiver);
+		when(transfer.getAmount()).thenReturn(new BigDecimal("50"));
+		
+		//WHEN
+		TransferDto dto = mapper.fromTransferToTransferDto(transfer, 2);
+		
+		BigDecimal expectedAmount = new BigDecimal("50.00");
+		
+		//THEN
+		assertEquals(expectedAmount, dto.amount());
+	}
+}
