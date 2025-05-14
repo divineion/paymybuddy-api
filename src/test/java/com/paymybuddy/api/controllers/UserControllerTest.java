c5234051f99a1774764275d45dedd87a18e8567e
divineion <najima.guermoudi@gmail.com>
2025-05-14 20:01:38 +0200

    feat(test): add UserController test methods
    Add 3 test methods:
     - `testGetUserById_ShouldReturnUserNotFound()` and
    `testTransferPage_shouldReturnNotFound()`: verify that the corresponding
    controller's methods return a 404 Not Found status with an empty body if
    no user is found.
     - method `testGetTransferPage_shouldReturnTransferPageDto()`: verifies
    that the controller returns a 200 OK status and that the JSON response
    exists.

--- src/test/java/com/paymybuddy/api/controllers/UserControllerTest.java
+++ src/test/java/com/paymybuddy/api/controllers/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.paymybuddy.api.controllers;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -14,6 +15,8 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.paymybuddy.api.config.SecurityConfig;
+import com.paymybuddy.api.exceptions.UserNotFoundException;
+import com.paymybuddy.api.model.dto.TransferPageDto;
 import com.paymybuddy.api.model.dto.UserDto;
 import com.paymybuddy.api.services.finder.UserFinderService;
 
@@ -29,21 +32,66 @@ public class UserControllerTest {
 	private UserFinderService finder;
 
 	@Test
-	public void shouldReturnUserDto() throws Exception {
+	public void testGetUserById_ShouldReturnUserDto() throws Exception {
 		// GIVEN
-		UserDto mockUserDto = new UserDto(1, "Sylvia", "sylvia@email.com");
+		int userId = 1;
+		UserDto mockUserDto = new UserDto(userId, "Sylvia", "sylvia@email.com");
 
 		// WHEN
-		when(finder.findUserById(1)).thenReturn(mockUserDto);
+		when(finder.findUserById(userId)).thenReturn(mockUserDto);
 
-		// ASSERT
+		// THEN
 		// https://www.javadoc.io/doc/org.springframework/spring-test/5.2.7.RELEASE/org/springframework/test/web/servlet/ResultActions.html
-		mockMvc.perform(get("/api/user/1"))
+		mockMvc.perform(get("/api/user/{userId}", userId))
 		.andExpectAll(
 			status().isOk(), 
 			jsonPath("$.id", is(1)),
 			jsonPath("$.username", is("Sylvia")), 
 			jsonPath("$.email", is("sylvia@email.com"))
 		);
+	}
+	
+	@Test
+	public void testGetUserById_ShouldReturnUserNotFound() throws Exception {
+		// GIVEN
+		int userId = 1;
+		
+		// WHEN
+		when(finder.findUserById(userId)).thenThrow(UserNotFoundException.class);
+		
+		//THEN
+		mockMvc.perform(get("/api/user/{userId}", userId))
+		.andExpect(status().isNotFound())
+		.andExpect(jsonPath("$").doesNotExist());
+	}
+	
+	@Test
+	public void testGetTransferPage_shouldReturnTransferPageDto() throws Exception {
+	    // GIVEN
+	    int userId = 1;
+	    // création d'un mock vide
+	    TransferPageDto mockTransferPageDto = mock(TransferPageDto.class);
+	    when(finder.findUserTransferPageInfo(userId)).thenReturn(mockTransferPageDto);
+
+	    // WHEN
+	    mockMvc.perform(get("/api/user/{id}/transfers", userId))
+	    
+	    // THEN
+	    .andExpect(status().isOk())
+	    .andExpect(jsonPath("$").exists());
+	}
+	
+	@Test
+	public void testTransferPage_shouldReturnNotFound() throws Exception {
+	    // GIVEN
+	    int userId = 1;
+	    when(finder.findUserTransferPageInfo(userId)).thenThrow(UserNotFoundException.class);
+
+	    // WHEN
+	    mockMvc.perform(get("/api/user/{id}/transfers", userId))
+	    
+	    // THEN
+	    .andExpect(status().isNotFound())
+	    .andExpect(jsonPath("$").doesNotExist());
 	}
 }
