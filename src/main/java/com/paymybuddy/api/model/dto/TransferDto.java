4218a6ccacce3b9209531525457e96a90dcfaa0c
divineion <najima.guermoudi@gmail.com>
2025-05-14 14:01:40 +0200

    fix(date format): format date in TransferDto
    Add Jackson annotation `@JsonFormat` to format the date field according
    to the format in the DB (yyyy-MM-dd HH:mm:ss)

--- src/main/java/com/paymybuddy/api/model/dto/TransferDto.java
+++ src/main/java/com/paymybuddy/api/model/dto/TransferDto.java
@@ -3,6 +3,8 @@ package com.paymybuddy.api.model.dto;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public record TransferDto(int id, BeneficiaryDto sender, BeneficiaryDto receiver, String description, BigDecimal amount,
-		LocalDateTime date) {
+		 @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime date) {
 }
