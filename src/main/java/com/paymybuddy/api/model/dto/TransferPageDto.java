12141b4b23022d3e4c3587e27a8c0afedad9b368
divineion <najima.guermoudi@gmail.com>
2025-05-14 12:02:50 +0200

    feat(endpoint): add `/api/user{}/transfers` endpoint
    TransferPageDto.java:
    Change beneficiaries from List to Set for a better JSON compatibility.

--- src/main/java/com/paymybuddy/api/model/dto/TransferPageDto.java
+++ src/main/java/com/paymybuddy/api/model/dto/TransferPageDto.java
@@ -2,8 +2,7 @@ package com.paymybuddy.api.model.dto;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Set;
 
-public record TransferPageDto(int id, Set<BeneficiaryDto> beneficiaries, BigDecimal balance,
+public record TransferPageDto(int id, List<BeneficiaryDto> beneficiaries, BigDecimal balance,
 		List<TransferDto> sentTransfers, List<TransferDto> receivedTransfers) {
 }
