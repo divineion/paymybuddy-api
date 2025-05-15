b5568c848668c8b16c66a952e511492378fb6ccf
divineion <najima.guermoudi@gmail.com>
2025-05-15 16:54:19 +0200

    feat(user): add custom exceptions

---
+++ src/main/java/com/paymybuddy/api/exceptions/EmailNotFoundException.java
@@ -0,0 +1,14 @@
+package com.paymybuddy.api.exceptions;
+
+public class EmailNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmailNotFoundException(String message) {
+		super(message);
+	}
+	
+}
