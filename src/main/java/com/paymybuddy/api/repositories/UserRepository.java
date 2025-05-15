cb3c6dba24c512f6635d35f14c5cc621a4b12ac0
divineion <najima.guermoudi@gmail.com>
2025-05-15 16:38:16 +0200

    feat(user): add repository method to find user by active email
    Add `findByActiveEmail(String email)` that returns an Optional<User> to
    safely handle the presence of absence of user.

--- src/main/java/com/paymybuddy/api/repositories/UserRepository.java
+++ src/main/java/com/paymybuddy/api/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.paymybuddy.api.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.paymybuddy.api.model.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+	Optional<User> findByActiveEmail(String email);
 
 }
