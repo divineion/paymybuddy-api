3c3d6229d65330ed5a64b458372f822ed2761d94
divineion <najima.guermoudi@gmail.com>
2025-05-15 16:58:20 +0200

    feat(endpoint): add /api/user/{id}/add-relation endpoint
    In `createRelation(@PathVariable int id, @RequestBody EmailRequestDto
    email)`:
     - use UserService to find and return a UserDto,
     - handle exceptions,
     - return proper ResponseEntity

--- src/main/java/com/paymybuddy/api/controllers/UserController.java
+++ src/main/java/com/paymybuddy/api/controllers/UserController.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.paymybuddy.api.exceptions.EmailNotFoundException;
+import com.paymybuddy.api.exceptions.SelfRelationException;
 import com.paymybuddy.api.exceptions.UserNotFoundException;
+import com.paymybuddy.api.model.dto.EmailRequestDto;
 import com.paymybuddy.api.model.dto.TransferPageDto;
 import com.paymybuddy.api.model.dto.UserDto;
 import com.paymybuddy.api.services.user.UserService;
@@ -14,13 +19,13 @@ import com.paymybuddy.api.services.user.UserService;
 @RestController
 public class UserController {
 	@Autowired
-	UserService finder;
+	UserService service;
 	
 	@GetMapping("/api/user/{id}")
 	public ResponseEntity<UserDto> getUserById(@PathVariable int id) {
 		UserDto user;
 		try {
-			user = finder.findUserById(id);
+			user = service.findUserById(id);
 			return ResponseEntity.ok(user);
 		} catch (UserNotFoundException e) {
 			return ResponseEntity.status(404).build();
@@ -31,10 +36,22 @@ public class UserController {
 	public ResponseEntity<TransferPageDto> getTransferPage(@PathVariable int id) {
 		TransferPageDto transferPageInfo;
 		try {
-			transferPageInfo = finder.findUserTransferPageInfo(id);
+			transferPageInfo = service.findUserTransferPageInfo(id);
 			return ResponseEntity.ok(transferPageInfo);
 		} catch(UserNotFoundException e) {
 			return ResponseEntity.status(404).build();
+		}
+	}
+	
+	@PostMapping("/api/user/{id}/add-relation")
+	public ResponseEntity<UserDto> createRelation(@PathVariable int id, @RequestBody EmailRequestDto email) {
+		try {
+			UserDto user = service.addBeneficiary(id, email);
+			return ResponseEntity.ok(user);
+		} catch (EmailNotFoundException e) {
+			return ResponseEntity.status(404).build();
+		} catch (SelfRelationException e) {
+			return ResponseEntity.status(400).build();
 		}
 	}
 }
