71189dbdad66e87900e99fc7ba50d18eca12023c
divineion <najima.guermoudi@gmail.com>
2025-05-16 18:28:07 +0200

    feat(user endpoint): handle exception
    Catch RelationAlreadyExistsException and return HTTP 409 Conflict.

--- src/main/java/com/paymybuddy/api/controllers/UserController.java
+++ src/main/java/com/paymybuddy/api/controllers/UserController.java
@@ -1,14 +1,17 @@
 package com.paymybuddy.api.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.paymybuddy.api.exceptions.EmailNotFoundException;
+import com.paymybuddy.api.exceptions.RelationAlreadyExistsException;
 import com.paymybuddy.api.exceptions.SelfRelationException;
 import com.paymybuddy.api.exceptions.UserNotFoundException;
 import com.paymybuddy.api.model.dto.EmailRequestDto;
@@ -18,6 +21,7 @@ import com.paymybuddy.api.services.user.UserService;
 
 @RestController
 public class UserController {
+	private static Logger logger = LogManager.getLogger(UserController.class);
 	@Autowired
 	UserService service;
 	
@@ -43,15 +47,19 @@ public class UserController {
 		}
 	}
 	
-	@PostMapping("/api/user/{id}/add-relation")
-	public ResponseEntity<UserDto> createRelation(@PathVariable int id, @RequestBody EmailRequestDto email) {
+	@PutMapping("/api/user/{id}/add-relation")
+	public ResponseEntity<UserDto> createRelation(@PathVariable int id, @RequestBody EmailRequestDto email) throws RelationAlreadyExistsException, UserNotFoundException {
 		try {
 			UserDto user = service.addBeneficiary(id, email);
 			return ResponseEntity.ok(user);
 		} catch (EmailNotFoundException e) {
 			return ResponseEntity.status(404).build();
 		} catch (SelfRelationException e) {
+			logger.error(e.getMessage());
 			return ResponseEntity.status(400).build();
+		} catch (RelationAlreadyExistsException e) {
+			logger.error(e.getMessage());
+			return ResponseEntity.status(409).build();
 		}
 	}
 }
