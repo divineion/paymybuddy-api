3f1f6f376854df3bce6978f10c5bed3552d7f447
divineion <najima.guermoudi@gmail.com>
2025-05-16 15:56:55 +0200

    feat(user): add RelationAlreadyExistsException
    Add an exception to handle attempt to add a beneficiary that already
    exists in user's beneficiaries list.

---
+++ src/main/java/com/paymybuddy/api/exceptions/RelationAlreadyExistsException.java
@@ -0,0 +1,12 @@
+package com.paymybuddy.api.exceptions;
+
+public class RelationAlreadyExistsException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public RelationAlreadyExistsException(String message) {
+		super(message);
+	}
+}
