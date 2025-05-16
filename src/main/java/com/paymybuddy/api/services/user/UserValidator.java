c27262f5e4d88d7ae2e65934ddca14f32c8831a0
divineion <najima.guermoudi@gmail.com>
2025-05-16 17:34:28 +0200

    feat(user): add business validation to prevent self as beneficiary
    Add method `validateUserCanAddAsBeneficiary()` in UserValidator to
    prevent a user adding himself as beneficiary.
    Throws SelfRelationException.
    
    Move `validateEmailExists()` method to UserService.

--- src/main/java/com/paymybuddy/api/services/user/UserValidator.java
+++ src/main/java/com/paymybuddy/api/services/user/UserValidator.java
@@ -2,25 +2,17 @@ package com.paymybuddy.api.services.user;
 
 import org.springframework.stereotype.Service;
 
-import com.paymybuddy.api.exceptions.EmailNotFoundException;
 import com.paymybuddy.api.exceptions.SelfRelationException;
 import com.paymybuddy.api.model.User;
-import com.paymybuddy.api.repositories.UserRepository;
 
 @Service
 public class UserValidator {
-	private final UserRepository repository;
-	
-	private UserValidator(UserRepository repository) {
-		this.repository = repository;
-	}
 
-	public User validateEmailExists(int id, String email) throws EmailNotFoundException, SelfRelationException {
-		User user = repository.findByActiveEmail(email).orElseThrow(() -> new EmailNotFoundException("the provided email doesn't match any user"));
-		if (user.getId() == id) {
-			throw new SelfRelationException("You cannot add yourself as a beneficiary.");
+	public boolean validateUserCanAddAsBeneficiary(int currentUserId, User targetUser) throws SelfRelationException {
+		if (targetUser.getId() == currentUserId) {
+			throw new SelfRelationException("The user cannot add himself as a beneficiary.");
 		}
-		return user;
+		return true;
 	}
 	
 }
