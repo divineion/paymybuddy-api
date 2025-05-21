55b8aee035e7310a92a92eeca9c18cd2717c642a
divineion <najima.guermoudi@gmail.com>
2025-05-21 20:57:07 +0200

    feat(exception): update Controller
    Throw InsufficientAmountException.

--- src/main/java/com/paymybuddy/api/controllers/TransferController.java
+++ src/main/java/com/paymybuddy/api/controllers/TransferController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.paymybuddy.api.exceptions.InsufficientAmountException;
 import com.paymybuddy.api.exceptions.InsufficientBalanceException;
 import com.paymybuddy.api.exceptions.UserNotFoundException;
 import com.paymybuddy.api.services.dto.TransferDto;
@@ -21,7 +22,7 @@ public class TransferController {
 	}
 	
 	@PostMapping("/api/transfer")
-	public ResponseEntity<TransferDto> createTransfer(@RequestBody TransferRequestDto transferReqDto) throws UserNotFoundException, InsufficientBalanceException {
+	public ResponseEntity<TransferDto> createTransfer(@RequestBody TransferRequestDto transferReqDto) throws UserNotFoundException, InsufficientBalanceException, InsufficientAmountException {
 		TransferDto transferDto = service.createTransfer(transferReqDto);
 		return ResponseEntity.ok(transferDto);
 	}
