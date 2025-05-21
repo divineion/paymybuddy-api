814e47fd62b55da6c212431ac5cf1f992cb36f8f
divineion <najima.guermoudi@gmail.com>
2025-05-21 18:11:36 +0200

    feat(endpoint): add POST "/api/transfer" endpoint
    TransferController: add file.
    Add method `createTransfer()`:
     - accepts a TransferRequestDto as the request body,
     - calls TransferService.createTransfer(),
     - returns a TransferDto if the transfer is successfully persisted,
     - throws an Exception otherwise.

---
+++ src/main/java/com/paymybuddy/api/controllers/TransferController.java
@@ -0,0 +1,29 @@
+package com.paymybuddy.api.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.paymybuddy.api.exceptions.InsufficientBalanceException;
+import com.paymybuddy.api.exceptions.UserNotFoundException;
+import com.paymybuddy.api.services.dto.TransferDto;
+import com.paymybuddy.api.services.dto.TransferRequestDto;
+import com.paymybuddy.api.services.transfer.TransferService;
+
+@RestController
+public class TransferController {
+	
+	private final TransferService service;
+	
+	public TransferController(TransferService service) {
+		this.service = service;
+	}
+	
+	@PostMapping("/api/transfer")
+	public ResponseEntity<TransferDto> createTransfer(@RequestBody TransferRequestDto transferReqDto) throws UserNotFoundException, InsufficientBalanceException {
+		TransferDto transferDto = service.createTransfer(transferReqDto);
+		return ResponseEntity.ok(transferDto);
+	}
+	
+}
