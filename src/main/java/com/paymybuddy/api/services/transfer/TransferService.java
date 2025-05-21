a80ed1cad82a4aa29f5c1cfa12c9dfe01f34da8f
divineion <najima.guermoudi@gmail.com>
2025-05-21 18:17:59 +0200

    feat(transfer): create full TransferService
    Add createTransfer() method to handle transfer creation:
    - fetch users from UserService,
    - validate balance,
    - map DTOs to entities,
    - save transfer and update user balances,
    - return a TransferDto.

---
+++ src/main/java/com/paymybuddy/api/services/transfer/TransferService.java
@@ -0,0 +1,73 @@
+package com.paymybuddy.api.services.transfer;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.paymybuddy.api.constants.ApiMessages;
+import com.paymybuddy.api.constants.ServiceFees;
+import com.paymybuddy.api.exceptions.InsufficientBalanceException;
+import com.paymybuddy.api.exceptions.UserNotFoundException;
+import com.paymybuddy.api.model.Transfer;
+import com.paymybuddy.api.model.User;
+import com.paymybuddy.api.repositories.TransferRepository;
+import com.paymybuddy.api.repositories.UserRepository;
+import com.paymybuddy.api.services.dto.TransferDto;
+import com.paymybuddy.api.services.dto.TransferRequestDto;
+import com.paymybuddy.api.services.dto.UserDto;
+import com.paymybuddy.api.services.user.UserMapper;
+import com.paymybuddy.api.services.user.UserService;
+
+@Service
+public class TransferService {
+
+	private final TransferMapper mapper;
+	private final TransferRepository repository;
+	private final UserService userService;
+	private final UserMapper userMapper;
+	
+	@Autowired
+	UserRepository userRepository;
+
+	public TransferService(TransferMapper mapper, TransferRepository repository, UserService userService, UserMapper userMapper) {
+		this.mapper = mapper;
+		this.repository = repository;
+		this.userService = userService;
+		this.userMapper = userMapper;
+	}
+
+	@Transactional(rollbackFor = Exception.class)
+	public TransferDto createTransfer(TransferRequestDto transferReqDto) throws UserNotFoundException, InsufficientBalanceException {
+		// FETCH SENDER infos
+		UserDto senderDto = userService.findUserById(transferReqDto.senderId());
+		// business validation
+		if (transferReqDto.amount().compareTo(senderDto.balance()) == 1) {
+			throw new InsufficientBalanceException(ApiMessages.INSUFFICIENT_BALANCE);
+		}
+		
+		// FETCH RECEIVER INFOS
+		UserDto receiverDto = userService.findUserById(transferReqDto.receiverId());
+		
+		// map dtos to user entities
+		User sender = userMapper.fromUserDtoToUser(senderDto);
+		User receiver = userMapper.fromUserDtoToUser(receiverDto);
+		
+		BigDecimal amount = transferReqDto.amount();
+		BigDecimal updatedSenderBalance = sender.getBalance().subtract(amount.add(amount.multiply(ServiceFees.TRANSFER_FEES)));
+		BigDecimal updatedReceiverBalance = receiver.getBalance().add(amount);
+
+		//map tranfer dto to transfer entity 
+		Transfer transfer = mapper.fromTransferDtoToTransfer(transferReqDto, sender, receiver);
+
+		repository.save(transfer);
+		userRepository.updateBalance(sender.getId(), updatedSenderBalance);
+		userRepository.updateBalance(receiver.getId(), updatedReceiverBalance);
+
+		TransferDto transferDto = mapper.fromTransferToTransferDto(transfer, transferReqDto.senderId());
+		
+		return transferDto;
+	}
+
+}
