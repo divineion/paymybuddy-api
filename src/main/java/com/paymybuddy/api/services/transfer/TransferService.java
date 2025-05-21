8377defa0cf8a43eea912fa14f77233301f48b9f
divineion <najima.guermoudi@gmail.com>
2025-05-21 20:55:57 +0200

    fix(transfer): amount validation
    Update `createTransfer()` to:
     - validate the minimum transferred amount,
     - correct the user's balance comparison (the balance should be compared
    with the total amount).

--- src/main/java/com/paymybuddy/api/services/transfer/TransferService.java
+++ src/main/java/com/paymybuddy/api/services/transfer/TransferService.java
@@ -2,12 +2,12 @@ package com.paymybuddy.api.services.transfer;
 
 import java.math.BigDecimal;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.paymybuddy.api.constants.ApiMessages;
-import com.paymybuddy.api.constants.ServiceFees;
+import com.paymybuddy.api.constants.TransferSettings;
+import com.paymybuddy.api.exceptions.InsufficientAmountException;
 import com.paymybuddy.api.exceptions.InsufficientBalanceException;
 import com.paymybuddy.api.exceptions.UserNotFoundException;
 import com.paymybuddy.api.model.Transfer;
@@ -19,6 +19,7 @@ import com.paymybuddy.api.services.dto.TransferRequestDto;
 import com.paymybuddy.api.services.dto.UserDto;
 import com.paymybuddy.api.services.user.UserMapper;
 import com.paymybuddy.api.services.user.UserService;
+import com.paymybuddy.api.utils.AmountUtils;
 
 @Service
 public class TransferService {
@@ -27,24 +28,28 @@ public class TransferService {
 	private final TransferRepository repository;
 	private final UserService userService;
 	private final UserMapper userMapper;
-	
-	@Autowired
-	UserRepository userRepository;
+	private final UserRepository userRepository;
 
-	public TransferService(TransferMapper mapper, TransferRepository repository, UserService userService, UserMapper userMapper) {
+
+	public TransferService(TransferMapper mapper, TransferRepository repository, UserService userService, UserMapper userMapper, UserRepository userRepository) {
 		this.mapper = mapper;
 		this.repository = repository;
 		this.userService = userService;
 		this.userMapper = userMapper;
+		this.userRepository = userRepository;
 	}
 
 	@Transactional(rollbackFor = Exception.class)
-	public TransferDto createTransfer(TransferRequestDto transferReqDto) throws UserNotFoundException, InsufficientBalanceException {
+	public TransferDto createTransfer(TransferRequestDto transferReqDto) throws UserNotFoundException, InsufficientBalanceException, InsufficientAmountException {
 		// FETCH SENDER infos
 		UserDto senderDto = userService.findUserById(transferReqDto.senderId());
-		// business validation
-		if (transferReqDto.amount().compareTo(senderDto.balance()) == 1) {
+		
+		if (AmountUtils.calculateAmountWithFees(transferReqDto.amount()).compareTo(senderDto.balance()) == 1) {
 			throw new InsufficientBalanceException(ApiMessages.INSUFFICIENT_BALANCE);
+		}
+		
+		if (transferReqDto.amount().compareTo(TransferSettings.MIN_TRANSFER_AMOUNT) == -1) {
+			throw new InsufficientAmountException(ApiMessages.INSUFFICIENT_AMOUNT);
 		}
 		
 		// FETCH RECEIVER INFOS
@@ -55,7 +60,7 @@ public class TransferService {
 		User receiver = userMapper.fromUserDtoToUser(receiverDto);
 		
 		BigDecimal amount = transferReqDto.amount();
-		BigDecimal updatedSenderBalance = sender.getBalance().subtract(amount.add(amount.multiply(ServiceFees.TRANSFER_FEES)));
+		BigDecimal updatedSenderBalance = sender.getBalance().subtract(amount.add(amount.multiply(TransferSettings.TRANSFER_FEES)));
 		BigDecimal updatedReceiverBalance = receiver.getBalance().add(amount);
 
 		//map tranfer dto to transfer entity 
