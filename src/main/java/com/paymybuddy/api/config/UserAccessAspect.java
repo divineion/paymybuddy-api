6e09b291801ac4c36a8a41f9a2fd6e365ed772b0
divineion <najima.guermoudi@gmail.com>
2025-05-29 20:46:20 +0200

    feat(user-access): implement method `doUserOrAdminAccessCheck()`
    Implement `doUserOrAdminAccessCheck()` to:
     - retrieve the `HttpServletRequest`,
     - extract and decode the JWT token to get the user's `authorities`
    claim,
     - allow the request if the user has the `ROLE_ADMIN`,
     - otherwise, compare the path/user ID with the token user ID and allow
    access only if they match, otherwise throw  a
    `ForbiddenAccessException`.

--- src/main/java/com/paymybuddy/api/config/UserAccessAspect.java
+++ src/main/java/com/paymybuddy/api/config/UserAccessAspect.java
@@ -15,6 +15,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.paymybuddy.api.exceptions.ForbiddenAccessException;
+import com.paymybuddy.api.model.RoleName;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -28,6 +29,14 @@ public class UserAccessAspect {
 	public UserAccessAspect(JwtDecoder jwtDecoder) {
 		this.jwtDecoder = jwtDecoder;
 	}
+	
+	private String validateAndFormatToken(String token) {
+		if (token != null && token.startsWith("Bearer ")) {
+			return token.substring(7);
+		} else {
+			throw new RuntimeException("Authorization header missing or malformed");
+		}
+	}
 
 	// TODO clean code
 	@Around(value = "@annotation(com.paymybuddy.api.annotations.AuthenticatedUser)")
@@ -39,12 +48,8 @@ public class UserAccessAspect {
 				.getRequest();
 
 		String token = request.getHeader("Authorization");
-
-		if (token != null && token.startsWith("Bearer ")) {
-			token = token.substring(7); // virer les 7 caractères qui correspondent à "Bearer
-		} else {
-			throw new RuntimeException("Authorization header missing or malformed");
-		}
+		
+		token = validateAndFormatToken(token);
 
 		Jwt decodedToken = jwtDecoder.decode(token);
 		long tokenUserId = decodedToken.getClaim("id");
@@ -58,6 +63,66 @@ public class UserAccessAspect {
 
 		Integer requestUserId = null;
 
+		for (int i = 0; i < paramNames.length; i++) {
+			// pathvariable id
+			if (paramNames[i].equals("id")) {
+				requestUserId = (Integer) arguments[i];
+				break;
+			}
+
+			// dto contenant une propriété id
+			Object argument = arguments[i];
+			if (argument != null) {
+				try {
+					// récup les informations ud champ id du dto passé en argument
+					Field idProp = argument.getClass().getDeclaredField("id");
+					idProp.setAccessible(true); // éviter l'IllegalAccessException
+					Object value = idProp.get(argument); // récup la valeur du champ
+					requestUserId = (Integer) value;
+					break;
+				} catch (Exception e) {
+					logger.error(e.getMessage());
+					throw e;
+				}
+			}
+		}
+
+		if (tokenUserId != requestUserId) {
+			logger.warn("Access denied: user {} tried to access resource of user {} at {}", 
+					tokenUserId, requestUserId, request.getRequestURI());
+			throw new ForbiddenAccessException("Forbidden access");
+		}
+
+		return pjp.proceed();
+	}
+
+	// TODO refactor
+	@Around(value = "@annotation(com.paymybuddy.api.annotations.AuthenticatedUserOrAdmin)")
+	public Object doUserOrAdminAccessCheck(ProceedingJoinPoint pjp) throws Throwable {
+		// retrieve users  role
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+				.getRequest();
+		
+		String token = request.getHeader("Authorization");
+		token = validateAndFormatToken(token);
+		
+		Jwt decodedToken = jwtDecoder.decode(token);
+		// suppressio des caractères autour du rôle
+		String tokenUserRole = decodedToken.getClaim("authorities").toString().replaceAll("[\\[\\]\"]", "");
+		long tokenUserId = decodedToken.getClaim("id");
+		
+		if (tokenUserRole.equals(RoleName.ROLE_ADMIN.toString())) {
+			logger.info("ADMIN (id: {}) initiated a soft-delete operation on a user via endpoint: {}", tokenUserId, request.getRequestURI());
+			return pjp.proceed();
+		}
+		
+		MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+		String[] paramNames = methodSignature.getParameterNames();
+		
+		Object[] arguments = pjp.getArgs();
+		
+		Integer requestUserId = null;
+		
 		for (int i = 0; i < paramNames.length; i++) {
 			// pathvariable id
 			if (paramNames[i].equals("id")) {
