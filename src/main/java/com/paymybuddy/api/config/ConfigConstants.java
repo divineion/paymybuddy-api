ccce9b2a6a2cecf08b2eefc2adbe5dd434427b95
divineion <najima.guermoudi@gmail.com>
2025-05-30 15:55:19 +0200

    feat(constants): add cookie max duration constants
    Add `DEV_ENV_COOKIE_DURATION` and `PROD_ENV_COOKIE_DURATION` constants
    to define
    cookie max-age values of 10 days for development environment and 1 hour
    for production.d 1 hour for production.

--- src/main/java/com/paymybuddy/api/config/ConfigConstants.java
+++ src/main/java/com/paymybuddy/api/config/ConfigConstants.java
@@ -2,4 +2,12 @@ package com.paymybuddy.api.config;
 
 public class ConfigConstants {
 	public static final String RSA_KEY_ID = "paymybuddy-keyID"; 
+	/**
+	 * Duration in seconds.
+	 */
+	public static final int DEV_ENV_COOKIE_DURATION_IN_SECONDS = 10 * 24 * 60 * 60;
+	/**
+	 * Duration in seconds.
+	 */
+	public static final int PROD_ENV_COOKIE_DURATION_IN_SECONDS = 1 * 60 * 60;
 }
