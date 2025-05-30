9170ff5f0812affd8e18a1c7d90e8d2084d2cba8
divineion <najima.guermoudi@gmail.com>
2025-05-30 12:47:23 +0200

    feat(controller): add admin password change endpoint
    Add PUT endpoint `/api/admin/user/{id}/change-password` accepting
    ChangePasswordDto in request body.
    Calls changePasswordAdminOnly() service method. Returns success message
    on completion.

--- src/main/java/com/paymybuddy/api/controllers/AdminController.java
+++ src/main/java/com/paymybuddy/api/controllers/AdminController.java
@@ -4,12 +4,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.paymybuddy.api.annotations.AdminOnly;
+import com.paymybuddy.api.constants.ApiMessages;
+import com.paymybuddy.api.exceptions.PasswordMissmatchException;
+import com.paymybuddy.api.exceptions.SamePasswordException;
 import com.paymybuddy.api.exceptions.UserDeletionNotAllowedException;
 import com.paymybuddy.api.exceptions.UserNotFoundException;
 import com.paymybuddy.api.exceptions.UserNotSoftDeletedException;
+import com.paymybuddy.api.services.dto.ApiResponse;
+import com.paymybuddy.api.services.dto.ChangePasswordDto;
 import com.paymybuddy.api.services.user.UserService;
 
 @RestController
@@ -25,5 +32,12 @@ public class AdminController {
 	public ResponseEntity<Void> deleteUser(@PathVariable int id) throws UserNotFoundException, UserNotSoftDeletedException, UserDeletionNotAllowedException {
 		service.deleteUser(id);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+	
+	@AdminOnly
+	@PutMapping("/api/admin/user/{id}/change-password")
+	public ResponseEntity<ApiResponse> updatePassword(@PathVariable int id, @RequestBody ChangePasswordDto newPasswordDto) throws UserNotFoundException, PasswordMissmatchException, SamePasswordException {
+		service.changePasswordAdminOnly(id, newPasswordDto);
+		return ResponseEntity.ok(new ApiResponse(ApiMessages.PASSWORD_SUCCESSFULLY_UPDATED));
 	}
 }
