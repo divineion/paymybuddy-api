bdc8bb629a1cf4d3aeea93c06a2bf6ac4067340f
divineion <najima.guermoudi@gmail.com>
2025-06-02 14:03:01 +0200

    feat(config): add `admin.default.password` to application.properties
    Add a default admin password property to simplify local setup for
    developers cloning the project.
    Document the property including its metadata.

--- src/main/java/com/paymybuddy/api/config/DataInitializer.java
+++ src/main/java/com/paymybuddy/api/config/DataInitializer.java
@@ -2,6 +2,7 @@ package com.paymybuddy.api.config;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,14 +16,27 @@ import com.paymybuddy.api.repositories.UserRepository;
 
 @Configuration
 public class DataInitializer {
+	@Value("${admin.default.password}")
+	private String adminPassword;
+
 	@Bean
-	CommandLineRunner createAdmin(UserRepository repository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
-	    return args -> {
-	        if (repository.findByActiveEmail("admin@email.com").isEmpty()) {
-	            Role adminRole = roleRepository.findByName(RoleName.ROLE_ADMIN).orElseThrow();
-	            User admin = User.forInitialData(null, "Admin", "admin@email.com", new BigDecimal("0"), passwordEncoder.encode("Admin_p@ssw0rd"), adminRole);
-	            repository.save(admin);
-	        }
-	    };
+	CommandLineRunner createAdmin(UserRepository repository, RoleRepository roleRepository,
+			PasswordEncoder passwordEncoder) {
+		System.out.println("adminPassword : " + adminPassword);
+
+		return args -> {
+			if (repository.findByActiveEmail("admin@email.com").isEmpty()) {
+				Role adminRole = roleRepository.findByName(RoleName.ROLE_ADMIN).orElseThrow();
+				User admin = User.forInitialData(null, "Admin", "admin@email.com", new BigDecimal("0"),
+						passwordEncoder.encode(adminPassword), adminRole);
+				
+				System.out.println("adminPassword : "+adminPassword);
+				System.out.println("type de  : "+adminPassword.getClass());
+
+				repository.save(admin);
+
+			}
+		};
 	}
+
 }
