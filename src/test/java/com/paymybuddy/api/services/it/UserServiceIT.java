43685b328b3a0ae8d7c28ae89055b65c01ed7f81
divineion <najima.guermoudi@gmail.com>
2025-06-11 04:17:10 +0200

    test(user-service): add integration tests for soft delete and delete user scenarios
    - Verify that soft deleting a user correctly sets the deletion
    timestamp.
    - Ensure deleting a user who is not soft-deleted throws
    UserNotSoftDeletedException.
    
    These tests improve coverage on user lifecycle management and exception
    handling.

---
+++ src/test/java/com/paymybuddy/api/services/it/UserServiceIT.java
@@ -0,0 +1,50 @@
+package com.paymybuddy.api.services.it;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.paymybuddy.api.exceptions.EmailNotFoundException;
+import com.paymybuddy.api.exceptions.UserAlreadySoftDeleted;
+import com.paymybuddy.api.exceptions.UserNotFoundException;
+import com.paymybuddy.api.exceptions.UserNotSoftDeletedException;
+import com.paymybuddy.api.model.User;
+import com.paymybuddy.api.repositories.UserRepository;
+import com.paymybuddy.api.services.user.UserService;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class UserServiceIT {
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Test
+	void TestSoftDeleteUser() throws UserNotFoundException, UserAlreadySoftDeleted, EmailNotFoundException {
+	        User user = userService.findUserByEmail("georgia@email.com");
+	        Assertions.assertNull(user.getDeletedAt());
+	        
+	        userService.softDeleteUser(user.getId());
+	        
+	        User deletedUser = userRepository.findById(user.getId()).orElseThrow();
+	        		
+	       Assertions.assertNotNull(deletedUser.getDeletedAt());
+	    }
+	
+	@Test
+	void TestDeleteUser_ShouldThrow() throws EmailNotFoundException {
+		User user = userService.findUserByEmail("georgia@email.com");
+			Assertions.assertNull(user.getDeletedAt());
+			
+			
+			
+			Assertions.assertThrows(UserNotSoftDeletedException.class, () -> userService.deleteUser(user.getId()));
+		
+	}
+
+}
